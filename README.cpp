c844f3985aa1b355ca01b43f5fb68f62e7213d4b
10woosung <105973818+10woosung@users.noreply.github.com>
2022-05-28 17:05:05 +0900

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -5,10 +5,11 @@
 
 using namespace std;
 
-struct node {
+struct node
+{
     int y, x;
 };
-
+//이 코드는 지렁이의 좌표역할을 한다.
 
 
 node snake[20] = { {3,3} };
@@ -20,7 +21,7 @@ void add(node xy) {
 char dir = 'R'; // 'U', 'D', 'L', 'R'
 int cnt = 0;
 
-int appleY, appleX;
+int appleY, appleX;//사과의 좌표변수 이다
 
 char MAP[20][21] = {
    "####################"  ,
@@ -44,7 +45,7 @@ char MAP[20][21] = {
    "#                  #"  ,
    "####################"  ,
 };
-
+//기본으로 출력되는 맵 역할을 한다.
 
 
 void printMap() { // MAP을 출력하는 기능 
@@ -57,7 +58,7 @@ void printMap() { // MAP을 출력하는 기능
     }
 }
 
-void eventcheck() {
+void eventcheck() { //지렁이의 이동방향을 바꾸는 기능
     if (GetAsyncKeyState(VK_UP)) {
         dir = 'U';
     }
@@ -73,7 +74,7 @@ void eventcheck() {
         dir = 'R';
     }
 }
-void move() {
+void move() { // 지렁이를 이동방향대로 이동시키는 기능
     if (dir == 'R') {
         snake[0].x++;
 
@@ -92,11 +93,11 @@ void move() {
     }
 }
 
-void appleSetting() {
+void appleSetting() { // 사과의 위치를 랜덤으로 변경시키는 기능
     appleY = rand() % 18 + 1; // 0~17   -> 1 ~ 18
     appleX = rand() % 18 + 1;
 }
-void follow() {
+void follow() { // 지렁이의 꼬리가 지렁이를 따라가게 하는 기능
     for (int i = tail - 1; i >= 1; i--) {
         snake[i] = snake[i - 1];
     }
@@ -108,13 +109,13 @@ int main()
         eventcheck();
         follow();
         move();
-        if (MAP[snake[0].y][snake[0].x] == '#') {
+        if (MAP[snake[0].y][snake[0].x] == '#') { // 지렁이가 맵 끝인 #에 닿았을 떄 게임오버가 되는 기능
             system("cls");
             printf("game over");
             Sleep(1000);
             break;
         }
-        for (int i = 1; i <= tail - 1; i++) {
+        for (int i = 1; i <= tail - 1; i++) { //지렁이가 꼬리에 닿았을 떄 게임오버가 되는 기능
             if ((snake[0].x == snake[i].x) && (snake[0].y == snake[i].y))
             {
                 system("cls");
@@ -124,23 +125,23 @@ int main()
 
             }
         }
-        MAP[snake[0].y][snake[0].x] = 'O';
-        MAP[appleY][appleX] = '@';
-        for (int i = 1; i <= tail - 1; i++) {
+        MAP[snake[0].y][snake[0].x] = 'O'; 
+        MAP[appleY][appleX] = '@'; 
+        for (int i = 1; i <= tail - 1; i++)  { // 지렁이의 꼬리를 생성하는 기능
             MAP[snake[i].y][snake[i].x] = 'o';
         }
 
         printMap();
 
-        MAP[snake[0].y][snake[0].x] = ' ';
-        MAP[appleY][appleX] = ' ';
-        for (int i = 0; i <= tail - 1; i++) {
+        MAP[snake[0].y][snake[0].x] = ' '; // 지렁이의 머리가 있던 자리를 없애주는 기능
+        MAP[appleY][appleX] = ' '; //사과가 있던 자리를 없애주는 기능
+        for (int i = 0; i <= tail - 1; i++) { // 꼬리가 있던 자리를 없애주는 기능
             MAP[snake[i].y][snake[i].x] = ' ';
         }
 
 
         printf("\n먹은 사과수%d개", cnt);
-        if (snake[0].x == appleX && appleY == snake[0].y) {
+        if (snake[0].x == appleX && appleY == snake[0].y) { //지렁이가 사과를 먹었을 때 속도를 증가시키고 사과를 랜덤좌표로 이동시키는 기능.
             cnt++;
             node temp = { snake[tail - 1].y , snake[tail - 1].x - 1 };
             add(temp);
@@ -161,3 +162,7 @@ int main()
 
     return 0;
 }
+
+
+
+
